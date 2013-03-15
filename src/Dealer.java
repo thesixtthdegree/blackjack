3a7a9fdb21859029b73270478d4e64639b659bf1
ar1o <ario.khosh@gmail.com>
2013-03-15 11:31:36 -0300

    dealer class

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,4 +1,40 @@
+import java.util.ArrayList;
 
-public class Dealer {
 
+
+public class Dealer extends Player
+{
+	private Hand hand; //the dealers hand
+	private Deck deck; //the deck the dealer deals from
+	
+	
+	//constructor	
+	public Dealer()
+	{
+		deck = new Deck(); //start deck object
+		hand = new Hand(); //start with empty hand object
+	}
+	
+
+	
+	//sets the number of cards to deal
+	public ArrayList<Card> deal(int n)
+	{
+		return deck.pick(n);
+		
+	}
+	//deal cards to the player based on number of cards to deal
+	public void deal(int n, Player player)
+	{
+		Hand playerHand = player.getHand();
+		playerHand.addToHand(deck.pick(n));
+		player.setHand(playerHand);
+	}
+	
+		
+	
+	
 }
+	
+	
+	
